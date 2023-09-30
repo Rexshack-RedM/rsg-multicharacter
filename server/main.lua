5182742c79fcafd0f8383d4284b39f39c089818c
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 14:25:08 +0100

    version checker

--- server/main.lua
+++ server/main.lua
@@ -1,8 +1,40 @@
+local RSGCore = exports['rsg-core']:GetCoreObject()
+
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-multicharacter/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
+
 -- Functions
 local identifierUsed = GetConvar('es_identifierUsed', 'steam')
 local foundResources = {}
 -- Functions
-local RSGCore = exports['rsg-core']:GetCoreObject()
 
 -- starter items
 local StarterItems = {
@@ -124,3 +156,8 @@ end, 'admin')
 RSGCore.Commands.Add("closeNUI", "Close Multi NUI", {}, false, function(source)
     TriggerClientEvent('rsg-multicharacter:client:closeNUI', source)
 end, 'user')
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
